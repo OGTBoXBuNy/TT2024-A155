b84092773a4b1b7f8e608a0d6d4f324abef5835f
BRYAN ALEJANDRO RAMIREZ DE LA CRUZ <bramirezd1500@alumno.ipn.mx>
2023-12-27 14:57:52 -0600

    Version 1.0.0, Initial Set Up itext7 and bouncy castle

--- TT2024-A155/Form1.Designer.cs
+++ TT2024-A155/Form1.Designer.cs
@@ -28,12 +28,186 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnSign = new Button();
+            btnGenLlaves = new Button();
+            btnPDF = new Button();
+            btnVerificar = new Button();
+            fileRoute = new SaveFileDialog();
+            txtpubKey = new TextBox();
+            txtprivKey = new TextBox();
+            btnpubKey = new Button();
+            btnprivKey = new Button();
+            btnBuscar = new Button();
+            txtRuta = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            openFile = new OpenFileDialog();
+            SuspendLayout();
+            // 
+            // btnSign
+            // 
+            btnSign.Location = new Point(157, 306);
+            btnSign.Name = "btnSign";
+            btnSign.Size = new Size(94, 29);
+            btnSign.TabIndex = 0;
+            btnSign.Text = "Sign";
+            btnSign.UseVisualStyleBackColor = true;
+            btnSign.Click += btnSign_Click;
+            btnSign.MouseCaptureChanged += btnSign_MouseCaptureChanged;
+            // 
+            // btnGenLlaves
+            // 
+            btnGenLlaves.Location = new Point(157, 113);
+            btnGenLlaves.Name = "btnGenLlaves";
+            btnGenLlaves.Size = new Size(142, 29);
+            btnGenLlaves.TabIndex = 1;
+            btnGenLlaves.Text = "Generar Llaves";
+            btnGenLlaves.UseVisualStyleBackColor = true;
+            btnGenLlaves.Click += btnGenLlaves_Click;
+            // 
+            // btnPDF
+            // 
+            btnPDF.Location = new Point(519, 210);
+            btnPDF.Name = "btnPDF";
+            btnPDF.Size = new Size(94, 29);
+            btnPDF.TabIndex = 2;
+            btnPDF.Text = "PDF";
+            btnPDF.UseVisualStyleBackColor = true;
+            btnPDF.Click += btnPDF_Click;
+            // 
+            // btnVerificar
+            // 
+            btnVerificar.Location = new Point(519, 320);
+            btnVerificar.Name = "btnVerificar";
+            btnVerificar.Size = new Size(94, 29);
+            btnVerificar.TabIndex = 3;
+            btnVerificar.Text = "Verificar";
+            btnVerificar.UseVisualStyleBackColor = true;
+            btnVerificar.Click += btnVerificar_Click;
+            // 
+            // txtpubKey
+            // 
+            txtpubKey.Location = new Point(157, 34);
+            txtpubKey.Name = "txtpubKey";
+            txtpubKey.Size = new Size(438, 27);
+            txtpubKey.TabIndex = 4;
+            // 
+            // txtprivKey
+            // 
+            txtprivKey.Location = new Point(157, 67);
+            txtprivKey.Name = "txtprivKey";
+            txtprivKey.Size = new Size(438, 27);
+            txtprivKey.TabIndex = 5;
+            // 
+            // btnpubKey
+            // 
+            btnpubKey.Location = new Point(619, 34);
+            btnpubKey.Name = "btnpubKey";
+            btnpubKey.Size = new Size(94, 29);
+            btnpubKey.TabIndex = 6;
+            btnpubKey.Text = "...";
+            btnpubKey.UseVisualStyleBackColor = true;
+            btnpubKey.Click += btnpubKey_Click;
+            // 
+            // btnprivKey
+            // 
+            btnprivKey.Location = new Point(619, 67);
+            btnprivKey.Name = "btnprivKey";
+            btnprivKey.Size = new Size(94, 29);
+            btnprivKey.TabIndex = 7;
+            btnprivKey.Text = "...";
+            btnprivKey.UseVisualStyleBackColor = true;
+            btnprivKey.Click += btnprivKey_Click;
+            // 
+            // btnBuscar
+            // 
+            btnBuscar.Location = new Point(619, 273);
+            btnBuscar.Name = "btnBuscar";
+            btnBuscar.Size = new Size(94, 29);
+            btnBuscar.TabIndex = 8;
+            btnBuscar.Text = "...";
+            btnBuscar.UseVisualStyleBackColor = true;
+            btnBuscar.Click += btnBuscar_Click;
+            // 
+            // txtRuta
+            // 
+            txtRuta.Location = new Point(157, 273);
+            txtRuta.Name = "txtRuta";
+            txtRuta.Size = new Size(438, 27);
+            txtRuta.TabIndex = 9;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(24, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(127, 20);
+            label1.TabIndex = 10;
+            label1.Text = "Ruta llave publica";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(24, 74);
+            label2.Name = "label2";
+            label2.Size = new Size(128, 20);
+            label2.TabIndex = 11;
+            label2.Text = "Ruta llave privada";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(59, 280);
+            label3.Name = "label3";
+            label3.Size = new Size(93, 20);
+            label3.TabIndex = 12;
+            label3.Text = "Ruta Archivo";
+            // 
+            // openFile
+            // 
+            openFile.FileName = "openFile";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtRuta);
+            Controls.Add(btnBuscar);
+            Controls.Add(btnprivKey);
+            Controls.Add(btnpubKey);
+            Controls.Add(txtprivKey);
+            Controls.Add(txtpubKey);
+            Controls.Add(btnVerificar);
+            Controls.Add(btnPDF);
+            Controls.Add(btnGenLlaves);
+            Controls.Add(btnSign);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnSign;
+        private Button btnGenLlaves;
+        private Button btnPDF;
+        private Button btnVerificar;
+        private SaveFileDialog fileRoute;
+        private TextBox txtpubKey;
+        private TextBox txtprivKey;
+        private Button btnpubKey;
+        private Button btnprivKey;
+        private Button btnBuscar;
+        private TextBox txtRuta;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private OpenFileDialog openFile;
     }
 }
--- TT2024-A155/Form1.cs
+++ TT2024-A155/Form1.cs
@@ -1,3 +1,15 @@
+extern alias NewBouncy;
+
+using NewBouncy::Org.BouncyCastle.Pqc.Crypto.Crystals.Dilithium;
+using NewBouncy::Org.BouncyCastle.Security;
+using iText.Commons.Bouncycastle.Crypto;
+using System.Security.Cryptography.X509Certificates;
+using iText.IO.Font.Constants;
+using iText.Kernel.Colors;
+using iText.Kernel.Font;
+using iText.Kernel.Pdf.Canvas;
+using System.Windows.Forms;
+
 namespace TT2024_A155
 {
     public partial class Form1 : Form
@@ -6,5 +18,386 @@ namespace TT2024_A155
         {
             InitializeComponent();
         }
+        byte[] byteArray;
+        static DilithiumPublicKeyParameters publicKey;
+        static DilithiumPrivateKeyParameters privateKey;
+        static byte[] signature;
+        static string PrettyPrint(byte[] bytes)
+        {
+            var base64 = Convert.ToBase64String(bytes);
+            //if (base64.Length > 50)
+            //return $"{base64[..25]}...{base64[^25..]}";
+
+            return base64;
+        }
+
+        private void btnSign_MouseCaptureChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSign_Click(object sender, EventArgs e)
+        {
+            //part 5
+            // sign
+            string path = @txtRuta.Text;
+            byte[] bytes = File.ReadAllBytes(path);
+            var alice = new DilithiumSigner();
+            alice.Init(true, privateKey);
+            signature = alice.GenerateSignature(bytes);
+
+            path = @"C:\Users\boxbu\Downloads\Dilithium\signature.txt";
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(PrettyPrint(signature));
+                }
+            }
+
+            MessageBox.Show($"Signature: {PrettyPrint(signature)}");
+        }
+
+        private void btnGenLlaves_Click(object sender, EventArgs e)
+        {
+            //part 2
+
+            var random = new SecureRandom();//GENERADOR DE NUMERO ALEATORIOS
+            var keyGenParameters = new DilithiumKeyGenerationParameters(random, DilithiumParameters.Dilithium3); //AES  128 bits
+            //part 3
+            var dilithiumKeyPairGenerator = new DilithiumKeyPairGenerator();
+            dilithiumKeyPairGenerator.Init(keyGenParameters);
+            var keyPair = dilithiumKeyPairGenerator.GenerateKeyPair();//Se obtiene la calve publica y privada
+
+
+
+            //part 4
+            // get and view the keys
+            //var publicKey = (DilithiumPublicKeyParameters)keyPair.Public;//CER 
+            //var privateKey = (DilithiumPrivateKeyParameters)keyPair.Private;//KEY 
+            publicKey = (DilithiumPublicKeyParameters)keyPair.Public;//CER 
+            privateKey = (DilithiumPrivateKeyParameters)keyPair.Private;//KEY 
+            var pubEncoded = publicKey.GetEncoded();
+            var privateEncoded = privateKey.GetEncoded();
+            string path = @"C:\Users\boxbu\Downloads\Dilithium\pubKey.txt";
+
+            // Create a file to write to.
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine(PrettyPrint(pubEncoded));
+            }
+
+            MessageBox.Show($"Public key: {PrettyPrint(pubEncoded)}");
+            path = @"C:\Users\boxbu\Downloads\Dilithium\privKey.txt";
+
+            // Create a file to write to.
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine(PrettyPrint(privateEncoded));
+            }
+
+            MessageBox.Show($"Private key: {PrettyPrint(privateEncoded)}");
+        }
+
+        private void btnPDF_Click(object sender, EventArgs e)
+        {
+            string ruta = Application.StartupPath + "\\VALE JEIC.pdf";
+            File.WriteAllBytes(ruta, Properties.Resources.VALE_JEIC);
+
+            fileRoute.InitialDirectory = @"C:\";
+            fileRoute.Title = "PEDIDO";
+            fileRoute.CheckPathExists = true;
+            fileRoute.DefaultExt = "pdf";
+            fileRoute.Filter = "PDF files (*.pdf)|*.pdf";
+            fileRoute.FilterIndex = 2;
+            fileRoute.RestoreDirectory = true;
+            fileRoute.FileName = "TEST PDF";
+            if (fileRoute.ShowDialog() == DialogResult.OK)
+            {
+
+                if (File.Exists(fileRoute.FileName))
+                {
+                    iText.Kernel.Pdf.PdfWriter pdfWriter = new iText.Kernel.Pdf.PdfWriter(fileRoute.FileName);
+
+
+                    iText.Kernel.Pdf.PdfReader pdfReader = new iText.Kernel.Pdf.PdfReader(Application.StartupPath + "\\VALE JEIC.pdf");
+
+
+                    iText.Kernel.Pdf.PdfDocument pdfdoc = new iText.Kernel.Pdf.PdfDocument(pdfReader, pdfWriter);
+
+
+
+
+
+                    for (int i = 0; i < 3; i++)
+                    {
+                        PdfCanvas canvas = new PdfCanvas(pdfdoc.GetPage(i + 1));
+                        iText.Kernel.Font.PdfFont font = PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD);
+
+                        int y = 633;
+                        int x = 109;
+                        int Items = 0;
+
+
+
+
+                        //HORA DE CREACIÓN DEL VALE
+                        canvas.BeginText().SetFontAndSize(font, 10)
+                                .MoveText(x - 77, y - 63)
+                                .SetFillColor(ColorConstants.RED)
+                                .ShowText("Creado: " + DateTime.Now.ToString())
+                                .EndText();
+
+
+
+                        //NUMERO DE ITEMS
+                        canvas.BeginText().SetFontAndSize(font, 9)
+                                    .MoveText(x - 16, 96)
+                                    .ShowText((Items + 1).ToString())
+                                    .EndText();
+                    }
+                    pdfdoc.Close();
+                    MessageBox.Show("Exitoso");
+
+
+                }
+                else
+                {
+                    iText.Kernel.Pdf.PdfWriter pdfWriter = new iText.Kernel.Pdf.PdfWriter(fileRoute.FileName);
+
+                    iText.Kernel.Pdf.PdfReader pdfReader = new iText.Kernel.Pdf.PdfReader(Application.StartupPath + "\\VALE JEIC.pdf");
+
+                    iText.Kernel.Pdf.PdfDocument pdfdoc = new iText.Kernel.Pdf.PdfDocument(pdfReader, pdfWriter);
+
+
+
+                    int NumeroFila = 4;
+
+                    for (int i = 0; i < 3; i++)
+                    {
+                        PdfCanvas canvas = new PdfCanvas(pdfdoc.GetPage(i + 1));
+                        iText.Kernel.Font.PdfFont font = PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD);
+
+                        int y = 633;
+                        int x = 109;
+                        int Items = 0;
+
+                        //HORA DE CREACIÓN DEL VALE
+                        canvas.BeginText().SetFontAndSize(font, 10)
+                                .MoveText(x - 77, y - 63)
+                                .SetFillColor(ColorConstants.RED)
+                                .ShowText("Creado: " + DateTime.Now.ToString())
+                                .EndText();
+
+
+
+                        //NUMERO DE ITEMS
+                        canvas.BeginText().SetFontAndSize(font, 9)
+                                    .MoveText(x - 16, 96)
+                                    .ShowText((Items + 1).ToString())
+                                    .EndText();
+                    }
+                    pdfdoc.Close();
+                    MessageBox.Show("Exitoso");
+
+
+                }
+
+
+
+            }
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                String path = @"C:\Users\boxbu\Downloads\Dilithium\signature.txt";
+                StreamReader sr = new StreamReader(path);
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                String temp = "";
+                while (line != null)
+                {
+                    temp += line;
+                    //write the line to console window
+                    Console.WriteLine(line);
+                    MessageBox.Show("La signature base 64 desde txt: " + line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+
+
+                //Recuperando la cadena se bytes de la signature  base64
+
+                signature = System.Convert.FromBase64String(temp);
+
+
+                MessageBox.Show("La signature en bytes longitud: " + signature.Length + "Cadena original:" + PrettyPrint(signature));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+
+
+            //part 6
+            // verify signature
+            var bob = new DilithiumSigner();
+            bob.Init(false, publicKey);
+
+            string fpath = @txtRuta.Text;
+            byteArray = File.ReadAllBytes(fpath);
+            //var data = Hex.Encode(byteArray);
+
+
+            var verified = bob.VerifySignature(byteArray, signature);
+            MessageBox.Show($"Successfully verified? {verified}");
+
+        }
+
+        private void btnpubKey_Click(object sender, EventArgs e)
+        {
+            //configuraciones del openfiledialog 1
+            openFile.InitialDirectory = "C:\\";
+            //openFile.Filter = " (*.*)|*.pdf*";
+            openFile.FilterIndex = 1;
+            openFile.RestoreDirectory = true;
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                txtpubKey.Text = openFile.FileName;
+                string fpath = @txtpubKey.Text;
+                byteArray = File.ReadAllBytes(fpath);
+                if (byteArray != null)
+                    MessageBox.Show("found public key");
+            }
+
+            String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader(openFile.FileName);
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                String temp = "";
+                while (line != null)
+                {
+                    temp += line;
+                    //write the line to console window
+                    Console.WriteLine(line);
+                    MessageBox.Show("Llave publica base 64 desde txt: " + line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+
+
+                //Recuperando la cadena se bytes de la llave publica base64
+
+                byte[] bytes = System.Convert.FromBase64String(temp);
+                publicKey = new DilithiumPublicKeyParameters(DilithiumParameters.Dilithium3, bytes);
+
+                MessageBox.Show("Llave publica en bytes longitud: " + bytes.Length + "Cadena original:" + PrettyPrint(bytes));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+
+        private void btnprivKey_Click(object sender, EventArgs e)
+        {
+            //configuraciones del openfiledialog 1
+            openFile.InitialDirectory = "C:\\";
+            openFile.Filter = " (*.*)|*.pdf*";
+            openFile.FilterIndex = 1;
+            openFile.RestoreDirectory = true;
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                txtprivKey.Text = openFile.FileName;
+                string fpath = @txtprivKey.Text;
+                byteArray = File.ReadAllBytes(fpath);
+                if (byteArray != null)
+                    MessageBox.Show("Found private key");
+            }
+
+            String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader(openFile.FileName);
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                String temp = "";
+                while (line != null)
+                {
+                    temp += line;
+                    //write the line to console window
+                    Console.WriteLine(line);
+                    MessageBox.Show("Llave privada base 64 desde txt: " + line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+
+
+                //Recuperando la cadena se bytes de la llave publica base64
+
+                byte[] bytes = System.Convert.FromBase64String(temp);
+                //privateKey = 
+
+                MessageBox.Show("Llave privada en bytes longitud: " + bytes.Length + "Cadena original:" + PrettyPrint(bytes));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            //configuraciones del openfiledialog 1
+            openFile.InitialDirectory = "C:\\";
+            //openFile.Filter = " (*.*)|*.pdf*";
+            openFile.FilterIndex = 1;
+            openFile.RestoreDirectory = true;
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                txtRuta.Text = openFile.FileName;
+                string fpath = @txtRuta.Text;
+                byteArray = File.ReadAllBytes(fpath);
+                if (byteArray != null)
+                    MessageBox.Show("found file");
+            }
+        }
     }
 }
+
+
+
+
